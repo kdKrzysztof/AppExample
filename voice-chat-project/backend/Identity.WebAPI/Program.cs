8d73f38265ec2fb8f938b83786b2ae6041211156
kdKrzysztof <57269917+kdKrzysztof@users.noreply.github.com>
2025-04-12 12:31:38 +0200

    feat: add migration + db

--- voice-chat-project/backend/Identity.WebAPI/Program.cs
+++ voice-chat-project/backend/Identity.WebAPI/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppDb"));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentityCore<IdentityUser>()
     .AddEntityFrameworkStores<AppDbContext>()
